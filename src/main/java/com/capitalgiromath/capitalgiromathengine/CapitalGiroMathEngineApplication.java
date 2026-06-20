1213218a8aa13d63e8bf1741413cffbd440f0ce1
Erick Rocha <erickamil@gmail.com>
2026-06-20 13:24:41 -0300

    Initial commit - Spring Boot skeleton

---
+++ src/main/java/com/capitalgiromath/capitalgiromathengine/CapitalGiroMathEngineApplication.java
@@ -0,0 +1,11 @@
+package com.capitalgiromath.capitalgiromathengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapitalGiroMathEngineApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CapitalGiroMathEngineApplication.class, args);
+    }
+}
